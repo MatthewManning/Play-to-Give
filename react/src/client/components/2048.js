6ff0f0978f4a85969cc03e4490204cec4c20be00
MatthewManning <matthewd.manning@gmail.com>
2018-04-16 12:53:50 -0500

    Changed class to className where appropriate.

--- react/src/client/components/2048.js
+++ react/src/client/components/2048.js
@@ -11,66 +11,66 @@ class Twenty48 extends Component{
   render(){
     return (
       <div>
-      <div class="container">
-          <div class="heading">
-            <h1 class="title">2048</h1>
-            <div class="scores-container">
-              <div class="score-container">0</div>
-              <div class="best-container">0</div>
+      <div className="container">
+          <div className="heading">
+            <h1 className="title">2048</h1>
+            <div className="scores-container">
+              <div className="score-container">0</div>
+              <div className="best-container">0</div>
             </div>
           </div>
 
-          <div class="above-game">
-            <p class="game-intro">Join the numbers and get to the <strong>2048 tile!</strong></p>
-            <a class="restart-button">New Game</a>
+          <div className="above-game">
+            <p className="game-intro">Join the numbers and get to the <strong>2048 tile!</strong></p>
+            <a className="restart-button">New Game</a>
           </div>
 
-          <div class="game-container">
-            <div class="game-message">
+          <div className="game-container">
+            <div className="game-message">
               <p></p>
-              <div class="lower">
-      	        <a class="keep-playing-button">Keep going</a>
-                <a class="retry-button">Try again</a>
+              <div className="lower">
+      	        <a className="keep-playing-button">Keep going</a>
+                <a className="retry-button">Try again</a>
               </div>
             </div>
 
-            <div class="grid-container">
-              <div class="grid-row">
-                <div class="grid-cell"></div>
-                <div class="grid-cell"></div>
-                <div class="grid-cell"></div>
-                <div class="grid-cell"></div>
+            <div className="grid-container">
+              <div className="grid-row">
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
               </div>
-              <div class="grid-row">
-                <div class="grid-cell"></div>
-                <div class="grid-cell"></div>
-                <div class="grid-cell"></div>
-                <div class="grid-cell"></div>
+              <div className="grid-row">
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
               </div>
-              <div class="grid-row">
-                <div class="grid-cell"></div>
-                <div class="grid-cell"></div>
-                <div class="grid-cell"></div>
-                <div class="grid-cell"></div>
+              <div className="grid-row">
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
               </div>
-              <div class="grid-row">
-                <div class="grid-cell"></div>
-                <div class="grid-cell"></div>
-                <div class="grid-cell"></div>
-                <div class="grid-cell"></div>
+              <div className="grid-row">
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
               </div>
             </div>
 
-            <div class="tile-container">
+            <div className="tile-container">
 
             </div>
           </div>
 
-          <p class="game-explanation">
-            <strong class="important">How to play:</strong> Use your <strong>arrow keys</strong> to move the tiles. When two tiles with the same number touch, they <strong>merge into one!</strong>
+          <p className="game-explanation">
+            <strong className="important">How to play:</strong> Use your <strong>arrow keys</strong> to move the tiles. When two tiles with the same number touch, they <strong>merge into one!</strong>
           </p>
           <p>
-          <strong class="important">Note:</strong> This site is the official version of 2048. You can play it on your phone via <a href="http://git.io/2048">http://git.io/2048.</a> All other apps or sites are derivatives or fakes, and should be used with caution.
+          <strong className="important">Note:</strong> This site is the official version of 2048. You can play it on your phone via <a href="http://git.io/2048">http://git.io/2048.</a> All other apps or sites are derivatives or fakes, and should be used with caution.
           </p>
           <p>
           Created by <a href="http://gabrielecirulli.com" target="_blank">Gabriele Cirulli.</a> Based on <a href="https://itunes.apple.com/us/app/1024!/id823499224" target="_blank">1024 by Veewo Studio</a> and conceptually similar to <a href="http://asherv.com/threes/" target="_blank">Threes by Asher Vollmer.</a>
